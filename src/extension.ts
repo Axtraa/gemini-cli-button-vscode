9143b4282a9048c4ff4075ffe9bcbe41f390f311
Lucas <lucas.benoit2000@gmail.com>
2026-01-16 16:00:24 +0100

    refactor: cleanup unused files and update package metadata

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Gemini CLI Button extension is active');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gemini-cli-button-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('gemini-button.run', () => {
-		// The code you place here will be executed every time your command is executed
 		const terminal = vscode.window.createTerminal({ name: `Gemini CLI`, location: { viewColumn: vscode.ViewColumn.Beside } });
 		terminal.sendText('gemini');
 		terminal.show();
@@ -23,5 +12,5 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
